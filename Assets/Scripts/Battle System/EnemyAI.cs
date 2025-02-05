5a6380251fbc88dd3283d7a18dde58e5c4558c56
Reyder95 <khoutas98@gmail.com>
2025-02-05 00:45:36 -0500

    Added mechanic null try catch block for situations where the AI initialization doesn't work right. Should not crash and instead end the turn afterwards.

--- Assets/Scripts/Battle System/EnemyAI.cs
+++ Assets/Scripts/Battle System/EnemyAI.cs
@@ -66,7 +66,7 @@ public class EnemyAI : MonoBehaviour
         catch (ArgumentOutOfRangeException ex)
         {
             InitializeAI(possibleTargets);
-            return ChooseAttack();
+            return null;
         }
 
     }
